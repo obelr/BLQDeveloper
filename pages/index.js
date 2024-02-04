b490edb80addaded7f97e897b7fd45efd4317b83
obel <rdebebe@kifiya.et>
2024-02-04 18:03:43 +0300

    hide slider

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <section className=" container">
        <Navbar />
-      <ImageSlider />
+      {/* <ImageSlider /> */}
       <Shortcuts/>
       <Discount/>
        </section>
